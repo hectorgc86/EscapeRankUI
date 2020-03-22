83ea67c7ec09a1ef7211424b5ceb3b7288c39b0d
Héctor GC <hectorgc86@gmail.com>
2020-03-22 12:35:18 +0100

    Login screen visual implementation

---
+++ EscapeRankUI/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using EscapeRankUI.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace EscapeRankUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new LoginPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
