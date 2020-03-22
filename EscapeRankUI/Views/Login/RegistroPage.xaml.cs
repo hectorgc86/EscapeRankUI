86c910d4d8a9ed3656fce01c548acaf79c5af105
Héctor GC <hectorgc86@gmail.com>
2020-03-22 15:00:44 +0100

    Minor changes

---
+++ EscapeRankUI/Views/Login/RegistroPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EscapeRankUI.ViewModels;
+using Xamarin.Forms;
+
+namespace EscapeRankUI.Views
+{
+    public partial class RegistroPage : ContentPage
+    {
+        public RegistroViewModel rvm;
+
+        public RegistroPage()
+        {
+            rvm = new RegistroViewModel(Navigation);
+            InitializeComponent();
+            BindingContext = rvm;
+
+
+        }
+    }
+}
